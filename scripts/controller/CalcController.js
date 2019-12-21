363aaab8561855943aecc1818a2597a2422505e9
marceloaagit <marcelo.maa@pc.ms.gov.br>
2019-12-21 14:47:13 -0400

    events click nos botoes

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,12 +1,14 @@
 class CalcController {
 
     constructor() {
+        this._operation = [];
         this._locale = 'pt-BR'
         this._displayCalcEl = document.querySelector("#display");
         this._dateEl = document.querySelector("#data");
         this._timeEl = document.querySelector("#hora");
         this._currentDate;
         this.initialize();
+        this.initButtonsEvents();
     }
 
     initialize() {
@@ -18,8 +20,30 @@ class CalcController {
 
     }
 
+    addEventListenerAll(element, events, fn){
+        events.split(' ').forEach(event => {
+            element.addEventListener(event, fn, false);
+        });
+    }
+
+    initButtonsEvents(){
+        let buttons = document.querySelectorAll('#buttons > g, #parts > g');
+        buttons.forEach((btn, index) => {
+            this.addEventListenerAll(btn, 'click drag', e =>{
+                console.log(btn.className.baseVal.replace('btn-', ''));
+            });
+            this.addEventListenerAll(btn, 'mouseover mouseup mousedown', e => {
+                btn.style.cursor = 'pointer';
+            });
+        });
+    }
+
     setDisplayDateTime(){
-        this.displayDate = this.currentDate.toLocaleDateString(this._locale);
+        this.displayDate = this.currentDate.toLocaleDateString(this._locale, {
+            day: '2-digit',
+            month: 'short',
+            year: 'numeric'
+        });
         this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
     }
 
