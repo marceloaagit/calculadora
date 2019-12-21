6e22b3e92a03c4e54bd1c23211c55042bf667b7b
marceloaagit <marcelo.maa@pc.ms.gov.br>
2019-12-21 15:42:28 -0400

    concatenação dos numérios

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -26,11 +26,104 @@ class CalcController {
         });
     }
 
+    clearAll(){
+        this._operation = [];
+    }
+
+    clearEntry(){
+        this._operation.pop();
+    }
+
+    setError(){
+        this.displayCalc = 'Error';
+    }
+
+    getLastOperation(){
+        return this._operation[this._operation.length - 1];
+    }
+
+    setLastOperation(value){
+        this._operation[this._operation.length - 1] = value;
+    }
+
+    isOperator(value){
+        return (['+', '-', '*', '/', '%'].indexOf(value) > -1);
+    }
+
+    addOperation(value){
+        if(isNaN(this.getLastOperation())){
+            //string
+            if(this.isOperator(value)){
+                //trocar operador
+                this.setLastOperation(value);
+            } else if(isNaN(value)) {
+                //outra coisa
+
+            } else {
+                this._operation.push(value);
+            }
+        } else {
+            //Number
+             let newValue = this.getLastOperation().toString() + value.toString();
+             this.setLastOperation(parseInt(newValue));
+        }
+        console.log(this._operation);
+    }
+
+    execBtn(value){
+        switch (value) {
+            case 'ac':
+                this.clearAll();
+                break;
+            case 'ce':
+                this.clearEntry();
+                break;
+            case 'soma':
+                this.addOperation('+');
+                break;
+            case 'igual':
+            
+                break;
+            case 'subtracao':
+            this.addOperation('-');
+                break;
+            case 'multiplicacao':
+            this.addOperation('*');
+                break;
+            case 'divisao':
+            this.addOperation('/');
+                break;
+            case 'porcento':
+            this.addOperation('%');
+                break;
+            case 'ponto':
+            this.addOperation('.');
+                break;
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':   
+            case '9':
+                this.addOperation(parseInt(value));
+                break;
+            
+            default:
+                this.setError();
+                break;
+        }
+    }
+
     initButtonsEvents(){
         let buttons = document.querySelectorAll('#buttons > g, #parts > g');
         buttons.forEach((btn, index) => {
             this.addEventListenerAll(btn, 'click drag', e =>{
-                console.log(btn.className.baseVal.replace('btn-', ''));
+                let textBtn = btn.className.baseVal.replace('btn-', '');
+                this.execBtn(textBtn);
             });
             this.addEventListenerAll(btn, 'mouseover mouseup mousedown', e => {
                 btn.style.cursor = 'pointer';
